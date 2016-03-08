9a3fac76330ebe471606b0366ab693b7f9dab7ba
Sebastian Wehrmann <sw@gocept.com>
2016-03-08 15:16:32 +0100

    Update build script

--- scripts/build_aqbanking.sh
+++ scripts/build_aqbanking.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
-AQBANKING_VERSION=${AQBANKING_VERSION-5.5.1}
-
+gwenhywfar_tarball_url = 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=78&file=01&dummy=gwenhywfar-4.13.1.tar.gz'
 aqbanking_tarball_url='http://www.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-5.5.1.tar.gz'
 
 temp_dir=$(mktemp -d /tmp/aqbanking.XXXXXXXXXX)
@@ -14,13 +12,22 @@ python -m SimpleHTTPServer $PORT &
 cd $temp_dir
 echo "Temp dir: $temp_dir"
 
+echo "Downloading $gwenhywfar_tarball_url"
+curl -L $gwenhywfar_tarball_url | tar xzv
+
 echo "Downloading $aqbanking_tarball_url"
 curl -L $aqbanking_tarball_url | tar xzv
 
+(
+    cd /tmp/gwenhywfar-4.13.1
+    ./configure --prefix=/tmp/gwenhywfar
+    make
+    make install
+)
 
 (
-	cd aqbanking-${NGINX_VERSION}
-	./configure --prefix=/tmp/aqbanking --with-gwen-dir=/app/gwenhywfar-4.13.0
+	cd /tmp/aqbanking-5.5.1
+	./configure --prefix=/tmp/aqbanking --with-gwen-dir=/tmp/gwenhywfar
     make
 	make install
 )
