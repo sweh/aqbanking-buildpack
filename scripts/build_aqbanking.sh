ac7f6dc2fe0771a98cbbc37e3429109886e54a9a
Sebastian Wehrmann <sw@gocept.com>
2016-03-08 16:33:51 +0100

    Try older gmp version

--- scripts/build_aqbanking.sh
+++ scripts/build_aqbanking.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-gwenhywfar_tarball_url = 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=78&file=01&dummy=gwenhywfar-4.13.1.tar.gz'
+gmp_tarball_url='http://ftp.gnu.org/gnu/gmp/gmp-4.3.2.tar.gz'
+gwenhywfar_tarball_url='http://www2.aquamaniac.de/sites/download/download.php?package=01&release=78&file=01&dummy=gwenhywfar-4.13.1.tar.gz'
 aqbanking_tarball_url='http://www.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-5.5.1.tar.gz'
 
-temp_dir=$(mktemp -d /tmp/aqbanking.XXXXXXXXXX)
 
-echo "Serving files from /tmp on $PORT"
+
+#echo "Serving files from /tmp on $PORT"
 cd /tmp
-python -m SimpleHTTPServer $PORT &
+#python -m SimpleHTTPServer $PORT &
 
-cd $temp_dir
-echo "Temp dir: $temp_dir"
+#cd $temp_dir
+#echo "Temp dir: $temp_dir"
+
+echo "Downloading $gmp_tarball_url"
+curl -L $gmp_tarball_url | tar xzv
 
 echo "Downloading $gwenhywfar_tarball_url"
 curl -L $gwenhywfar_tarball_url | tar xzv
@@ -18,22 +22,19 @@ curl -L $gwenhywfar_tarball_url | tar xzv
 echo "Downloading $aqbanking_tarball_url"
 curl -L $aqbanking_tarball_url | tar xzv
 
-(
-    cd /tmp/gwenhywfar-4.13.1
-    ./configure --prefix=/tmp/gwenhywfar
-    make
-    make install
-)
+cd /tmp/gmp-4.3.2
+./configure --prefix=/tmp/gmp
+make
+make install
 
-(
-	cd /tmp/aqbanking-5.5.1
-	./configure --prefix=/tmp/aqbanking --with-gwen-dir=/tmp/gwenhywfar
-    make
-	make install
-)
 
-while true
-do
-	sleep 1
-	echo "."
-done
+cd /tmp/gwenhywfar-4.13.1
+./configure --prefix=/tmp/gwenhywfar --with-guis=
+make
+make install
+
+
+cd /tmp/aqbanking-5.5.1
+./configure --prefix=/tmp/aqbanking --with-gwen-dir=/tmp/gwenhywfar CPPFLAGS="-I/tmp/gmp/include"
+make
+make install
