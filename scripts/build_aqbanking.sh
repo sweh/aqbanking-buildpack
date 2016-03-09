10b429b9a4915abd57090eb83fec02cdaabd00ad
Sebastian Wehrmann <sw@gocept.com>
2016-03-09 13:34:22 +0100

    Clean up build script

--- scripts/build_aqbanking.sh
+++ scripts/build_aqbanking.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-xmlsec1_tarball_url='http://www.aleksey.com/xmlsec/download/xmlsec1-1.2.20.tar.gz'
-gmp_tarball_url='http://ftp.gnu.org/gnu/gmp/gmp-4.3.2.tar.gz'
-gwenhywfar_tarball_url='http://www2.aquamaniac.de/sites/download/download.php?package=01&release=78&file=01&dummy=gwenhywfar-4.13.1.tar.gz'
-aqbanking_tarball_url='http://www.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-5.5.1.tar.gz'
+XMLSEC1_VERSION="1.2.20"
+GMP_VERSION="4.3.2"
+GWENHYWFAR_VERSION="4.13.1"
+AQBANKING_VERSION="5.5.1"
 
+xmlsec1_tarball_url="http://www.aleksey.com/xmlsec/download/xmlsec1-$XMLSEC1_VERSION.tar.gz"
+gmp_tarball_url="http://ftp.gnu.org/gnu/gmp/gmp-$GMP_VERSION.tar.gz"
+gwenhywfar_tarball_url="http://www2.aquamaniac.de/sites/download/download.php?package=01&release=78&file=01&dummy=gwenhywfar-$GWENHYWFAR_VERSION.tar.gz"
+aqbanking_tarball_url="http://www.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-$AQBANKING_VERSION.tar.gz"
 
+mkdir -p /app/download
+mkdir -p /app/.aqbanking-buildpack
 
-echo "Serving files from /app on $PORT"
-cd /app
+echo "Serving files from /app/download on $PORT"
+cd /app/download
 python -m SimpleHTTPServer $PORT &
 
-mkdir -p /app/.aqbanking-buildpack
 cd /tmp
-#cd $temp_dir
-#echo "Temp dir: $temp_dir"
 
 echo "Downloading $xmlsec1_tarball_url"
 curl -L $xmlsec1_tarball_url | tar xzv
@@ -29,28 +32,29 @@ echo "Downloading $aqbanking_tarball_url"
 curl -L $aqbanking_tarball_url | tar xzv
 
 
-cd /tmp/xmlsec1-1.2.20
+cd "/tmp/xmlsec1-$XMLSEC1_VERSION"
 ./configure --prefix=/app/.aqbanking-buildpack/xmlsec1
 make
 make install
 
-cd /tmp/gmp-4.3.2
+cd "/tmp/gmp-$GMP_VERSION"
 ./configure --prefix=/app/.aqbanking-buildpack/gmp
 make
 make install
 
-cd /tmp/gwenhywfar-4.13.1
+cd "/tmp/gwenhywfar-$GWENHYWFAR_VERSION"
 ./configure --prefix=/app/.aqbanking-buildpack/gwenhywfar --with-guis=
 make
 make install
 
-cd /tmp/aqbanking-5.5.1
+cd "/tmp/aqbanking-$AQBANKING_VERSION"
 XMLSEC_CFLAGS="$(/bin/bash /app/.aqbanking-buildpack/xmlsec1/bin/xmlsec1-config --cflags)" XMLSEC_LIBS="$(/bin/bash /app/.aqbanking-buildpack/xmlsec1/bin/xmlsec1-config --libs)" ./configure --prefix=/app/.aqbanking-buildpack/aqbanking --with-gwen-dir=/app/.aqbanking-buildpack/gwenhywfar gmp_libs="-L/app/.aqbanking-buildpack/gmp/lib -lgmp" CPPFLAGS="-I/app/.aqbanking-buildpack/gmp/include"
 make
 make install
 
 cd /app/
-tar -czf aqbanking.tar.gz .aqbanking-buildpack
+tar -czf "aqbanking-$AQBANKING_VERSION.tar.gz" .aqbanking-buildpack
+mv "aqbanking-$AQBANKING_VERSION.tar.gz" download
 
 while true
 do
